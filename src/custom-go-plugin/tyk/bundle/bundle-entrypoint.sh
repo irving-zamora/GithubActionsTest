5eb5100add33b9e9f92443bf8e8285cf900d00d3
Irving Zamora <70725961+irving-zamora@users.noreply.github.com>
2023-09-08 10:59:16 -0600

    Update bundle-entrypoint.sh

--- src/custom-go-plugin/tyk/bundle/bundle-entrypoint.sh
+++ src/custom-go-plugin/tyk/bundle/bundle-entrypoint.sh
@@ -12,6 +12,12 @@ cd /opt/tyk-gateway/bundle && /opt/tyk-gateway/tyk bundle build -y;
 # rename bundle zip to include gateway version targeted for and build version
 mv bundle.zip RateLimitingPlugin_gw5.0.3_1.0.1.zip
 
+# replace the path and file_list in manifest.json 
+# sed ...
+
+# also wondering if we should put this zip bundle artifact either back into github or aritfactory?
+# talk of artifactory being replaced
+
 # Cleanup
 rm /opt/tyk-gateway/bundle/RateLimitingPlugin*.so;
 
