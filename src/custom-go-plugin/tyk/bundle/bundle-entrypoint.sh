7a7074c15bc816954b3958eb4d951b611527c071
Irving Zamora <70725961+irving-zamora@users.noreply.github.com>
2023-09-08 10:25:48 -0600

    Update bundle-entrypoint.sh

--- src/custom-go-plugin/tyk/bundle/bundle-entrypoint.sh
+++ src/custom-go-plugin/tyk/bundle/bundle-entrypoint.sh
@@ -4,13 +4,16 @@ set -euo pipefail;
 echo "Building plugin bundle...";
 
 # Copy custom plugin to bundle directory
-cp /opt/tyk-gateway/middleware/RateLimitingPlugin.so /opt/tyk-gateway/bundle/RateLimitingPlugin.so;
+cp /opt/tyk-gateway/middleware/RateLimitingPlugin*.so /opt/tyk-gateway/bundle/RateLimitingPlugin.so;
 
 # Run bundler tool in bundle directory
 cd /opt/tyk-gateway/bundle && /opt/tyk-gateway/tyk bundle build -y;
 
+# rename bundle zip to include gateway version targeted for and build version
+mv bundle.zip RateLimitingPlugin_gw5.0.3_1.0.1.zip
+
 # Cleanup
-rm /opt/tyk-gateway/bundle/RateLimitingPlugin.so;
+rm /opt/tyk-gateway/bundle/RateLimitingPlugin*.so;
 
 # Exit
 echo "Done building plugin bundle.";
